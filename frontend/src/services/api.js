e7dd3a4bdd4c92cc3aaa3f4d552a9671998b1d7d
MatheusKristman <kristman058@gmail.com>
2023-03-21 11:36:02 -0300

    trying fix base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL:
-        import.meta.env.MODE === "development"
+        JSON.stringify(import.meta.env.MODE) === JSON.stringify("development")
             ? import.meta.env.VITE_API_KEY_DEV + import.meta.env.VITE_API_PORT
             : import.meta.env.VITE_API_KEY,
 });
