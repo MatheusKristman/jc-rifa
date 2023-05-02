c01f1a931c84e196eb49b9e24363a5004327e621
MatheusKristman <kristman058@gmail.com>
2023-05-02 15:09:12 -0300

    fixed app js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,77 +1,74 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import {
-    Home,
-    Raffle,
-    RaffleSelected,
-    QueryNumbers,
-    Register,
-    UpdateRegistration,
-    Winners,
-    Terms,
-    Contact,
-    RaffleManagement,
-    NewRaffle,
-    EditRaffle,
-} from './pages';
-import ProtectedRoute from './ProtectedRoute';
-import ProtectedRouteAdmin from './ProtectedRouteAdmin';
+  Home,
+  Raffle,
+  RaffleSelected,
+  QueryNumbers,
+  Register,
+  UpdateRegistration,
+  Winners,
+  Terms,
+  Contact,
+  RaffleManagement,
+  NewRaffle,
+  EditRaffle,
+} from "./pages";
+import ProtectedRoute from "./ProtectedRoute";
+import ProtectedRouteAdmin from "./ProtectedRouteAdmin";
 
-import './css/styles.css';
+import "./css/styles.css";
 
 function App() {
-    return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/raffles' element={<Raffle />} />
-                    <Route
-                        path='/raffles/:selected'
-                        element={<RaffleSelected />}
-                    />
-                    <Route path='/query-numbers' element={<QueryNumbers />} />
-                    <Route path='/register' element={<Register />} />
-                    <Route
-                        path='/updateRegistration'
-                        element={
-                            <ProtectedRoute>
-                                <UpdateRegistration />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route path='/winners' element={<Winners />} />
-                    <Route path='/terms' element={<Terms />} />
-                    <Route path='/contact' element={<Contact />} />
-                    <Route
-                        path='/raffle-management'
-                        element={
-                            <ProtectedRouteAdmin>
-                                <RaffleManagement />
-                            </ProtectedRouteAdmin>
-                        }
-                    />
-                    <Route
-                        path='/create-new-raffle'
-                        element={
-                            <ProtectedRouteAdmin>
-                                <NewRaffle />
-                            </ProtectedRouteAdmin>
-                        }
-                    />
-                    <Route
-                        path='/edit-raffle/:id'
-                        element={
-                            <ProtectedRouteAdmin>
-                                <EditRaffle />
-                            </ProtectedRouteAdmin>
-                        }
-                    />
-                </Routes>
-            </Router>
-        </>
-    );
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/raffles" element={<Raffle />} />
+          <Route path="/raffles/:selected" element={<RaffleSelected />} />
+          <Route path="/query-numbers" element={<QueryNumbers />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/updateRegistration"
+            element={
+              <ProtectedRoute>
+                <UpdateRegistration />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/winners" element={<Winners />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/raffle-management"
+            element={
+              <ProtectedRouteAdmin>
+                <RaffleManagement />
+              </ProtectedRouteAdmin>
+            }
+          />
+          <Route
+            path="/create-new-raffle"
+            element={
+              <ProtectedRouteAdmin>
+                <NewRaffle />
+              </ProtectedRouteAdmin>
+            }
+          />
+          <Route
+            path="/edit-raffle/:id"
+            element={
+              <ProtectedRouteAdmin>
+                <EditRaffle />
+              </ProtectedRouteAdmin>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
