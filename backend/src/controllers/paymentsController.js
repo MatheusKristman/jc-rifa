e792500916f574cf69fc8d0f49eb3f6a3180622b
MatheusKristman <kristman058@gmail.com>
2023-05-09 14:13:13 -0300

    fixed raffle image

--- backend/src/controllers/paymentsController.js
+++ backend/src/controllers/paymentsController.js
@@ -1,24 +1,26 @@
-const mercadopago = require("mercadopago");
-require("dotenv").config();
+const mercadopago = require('mercadopago');
+require('dotenv').config();
 
 console.log(process.env.MERCADO_PAGO_ACCESS_TOKEN);
 
-mercadopago.configurations.setAccessToken(process.env.MERCADO_PAGO_ACCESS_TOKEN);
+mercadopago.configurations.setAccessToken(
+    process.env.MERCADO_PAGO_ACCESS_TOKEN
+);
 
 module.exports = {
     pay: async (req, res) => {
         const paymentData = {
-            notification_url: "https://eoqhcniy7v8myie.m.pipedream.net",
+            notification_url: 'https://eoqhcniy7v8myie.m.pipedream.net',
             external_reference: req.body.id,
             transaction_amount: req.body.fullPrice,
             description: req.body.title,
-            payment_method_id: "pix",
+            payment_method_id: 'pix',
             payer: {
                 email: req.body.email,
                 first_name: req.body.firstName,
                 last_name: req.body.lastName,
                 identification: {
-                    type: "CPF",
+                    type: 'CPF',
                     number: req.body.cpf,
                 },
             },
@@ -30,3 +32,5 @@ module.exports = {
             .catch((error) => res.status(400).send({ error: error.message }));
     },
 };
+
+// TODO arrumar erro number identification
--- frontend/src/pages/components/raffle-management/RaffleManagementContent.jsx
+++ frontend/src/pages/components/raffle-management/RaffleManagementContent.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
-import { AiOutlinePlus } from "react-icons/ai";
-import { Link, useNavigate } from "react-router-dom";
-import api from "../../../services/api";
+import React, { useEffect } from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
+import api from '../../../services/api';
 
-import Prizes from "../Prizes";
-import useRaffleStore from "../../../stores/useRaffleStore";
-import _arrayBufferToBase64 from "../../../hooks/useArrayBufferToBase64";
-import Loading from "../Loading";
-import useGeneralStore from "../../../stores/useGeneralStore";
+import Prizes from '../Prizes';
+import useRaffleStore from '../../../stores/useRaffleStore';
+import _arrayBufferToBase64 from '../../../hooks/useArrayBufferToBase64';
+import Loading from '../Loading';
+import useGeneralStore from '../../../stores/useGeneralStore';
 
 const RaffleManagementContent = () => {
     const navigate = useNavigate();
@@ -17,7 +17,13 @@ const RaffleManagementContent = () => {
         setRaffles: state.setRaffles,
     }));
 
-    const { isLoading, setToLoad, setNotToLoad, setToAnimateFadeIn, setToAnimateFadeOut } = useGeneralStore((state) => ({
+    const {
+        isLoading,
+        setToLoad,
+        setNotToLoad,
+        setToAnimateFadeIn,
+        setToAnimateFadeOut,
+    } = useGeneralStore((state) => ({
         isLoading: state.isLoading,
         setToLoad: state.setToLoad,
         setNotToLoad: state.setNotToLoad,
@@ -30,7 +36,7 @@ const RaffleManagementContent = () => {
             setToLoad();
             setToAnimateFadeIn();
 
-            api.get("/raffle-management/get-raffles")
+            api.get('/raffle-management/get-raffles')
                 .then((res) => {
                     setRaffles(res.data);
 
@@ -59,44 +65,51 @@ const RaffleManagementContent = () => {
     };
 
     return (
-        <div className="raffle-management__content">
+        <div className='raffle-management__content'>
             {isLoading && <Loading>Carregando rifas</Loading>}
-            <div className="raffle-management__content__container">
-                <div className="raffle-management__content__container__info-wrapper">
-                    <h1 className="raffle-management__content__container__info-wrapper__title">⚙️ Gerenciador de Rifas</h1>
+            <div className='raffle-management__content__container'>
+                <div className='raffle-management__content__container__info-wrapper'>
+                    <h1 className='raffle-management__content__container__info-wrapper__title'>
+                        ⚙️ Gerenciador de Rifas
+                    </h1>
 
                     <button
-                        onClick={() => navigate("/create-new-raffle")}
-                        type="button"
-                        className="raffle-management__content__container__info-wrapper__add-btn"
+                        onClick={() => navigate('/create-new-raffle')}
+                        type='button'
+                        className='raffle-management__content__container__info-wrapper__add-btn'
                     >
-                        <AiOutlinePlus color="white" /> Criar
+                        <AiOutlinePlus color='white' /> Criar
                     </button>
                 </div>
 
-                <div className="raffle-management__content__container__raffle-wrapper">
-                    <ul className="raffle-management__content__container__raffle-wrapper__list">
+                <div className='raffle-management__content__container__raffle-wrapper'>
+                    <ul className='raffle-management__content__container__raffle-wrapper__list'>
                         {raffles.map((raffle) => (
                             <li
                                 key={raffle._id}
-                                className="raffle-management__content__container__raffle-wrapper__list__list-item"
+                                className='raffle-management__content__container__raffle-wrapper__list__list-item'
                             >
                                 <Link
                                     to={`/edit-raffle/${raffle._id}`}
-                                    className="raffle-management__content__container__raffle-wrapper__list__list-item__link"
+                                    className='raffle-management__content__container__raffle-wrapper__list__list-item__link'
                                 >
                                     <Prizes
                                         title={raffle.title}
                                         subtitle={raffle.subtitle}
                                         image={
                                             raffle.raffleImage.data
-                                                ? `data:${raffle.raffleImage.contentType};base64,${_arrayBufferToBase64(
-                                                      raffle.raffleImage.data.data
+                                                ? `data:${
+                                                      raffle.raffleImage
+                                                          .contentType
+                                                  };base64,${_arrayBufferToBase64(
+                                                      raffle.raffleImage.data
+                                                          .data
                                                   )}`
                                                 : null
                                         }
                                         progress={convertProgress(
-                                            raffle?.QuantNumbers - raffle?.NumbersAvailable.length,
+                                            raffle?.QuantNumbers -
+                                                raffle?.NumbersAvailable.length,
                                             raffle?.QuantNumbers
                                         )}
                                         winner={raffle?.isFinished}
@@ -112,3 +125,5 @@ const RaffleManagementContent = () => {
 };
 
 export default RaffleManagementContent;
+
+// TODO adicionar seção de promoção
